4ca5f1ae5f6c639fbe14d5efd5099cbe182609e0
ralphpark <ralphpark@knou.ac.kr>
2023-11-08 21:21:05 +0900

    flutter practice update

--- lib/receip_detail.dart
+++ lib/receip_detail.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/recipe_model.dart';
-class RecipeDetail extends StatefulWidget {
+class RecipeDetail extends StatefulWidget { // slider를 추가하기 위해 stateful widget을 사용
   final Recipe recipe;
   const RecipeDetail({Key? key, required this.recipe}) : super(key: key);
   @override
@@ -8,6 +8,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
+  double multiplier = 1; // slider의 기본값
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,13 +17,34 @@ class _RecipeDetailState extends State<RecipeDetail> {
         ),
         body: SafeArea(
             child: Column(
-              children: [
+          children: [
             SizedBox(
                 height: 300,
                 width: double.infinity,
                 child: Image.asset(widget.recipe.imageUrl)),
             const SizedBox(height: 16.0),
             Text(widget.recipe.label),
+            const SizedBox(height: 16.0),
+            Expanded( // 컬럼의 남은 공간을 다 차지해 주는 역할
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text('${(ingredient.quantity * multiplier)} ${ingredient.measure} ${ingredient.name}'); // listView는 리스트의 값을 하나씩 불러 온다.
+              },),
+            ),
+            Slider(value: multiplier, onChanged: (value) {
+              print(value);
+              setState(() { // setState를 통해 변경된 값을 적용
+                multiplier = value; // 변화되는 값을 setState 안이나 밖에 저장해도 동일하다.
+              });
+            },
+              min: 1,
+              max: 10,
+              divisions: 9, // slider의 구간을 나누어 준다. 1부터 10까지면 디비전은 9개
+              label: '${(widget.recipe.servings*multiplier).toInt()}인분', // slider의 값이 변할 때마다 나타나는 값
+            ),
           ],
         )));
   }
--- lib/recipe_model.dart
+++ lib/recipe_model.dart
@@ -2,33 +2,78 @@ class Recipe{
   String label;
   String imageUrl;
 
+  int servings;
+  List<Ingredient> ingredients;
+
   //TODO : 몇인분, 재료
-  Recipe(this.label, this.imageUrl);
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
 
   static List<Recipe> samples = [
     Recipe(
       'Spaghetti and Meatballs',
       'assets/spaghetti_meatballs.jpeg',
+      4,
+      [
+        Ingredient(1, 'box', 'Spaghetti'),
+      ],
     ),
     Recipe(
       'Tomato Soup',
       'assets/tomato_soup.jpeg',
+      2,
+      [
+        Ingredient(1, 'can', 'Tomato Soup'),
+        Ingredient(1, 'cup', 'Water'),
+
+      ],
     ),
     Recipe(
       'Grilled Cheese',
       'assets/grilled_cheese.jpeg',
+      1,
+      [
+        Ingredient(2, 'slices', 'Cheese'),
+        Ingredient(2, 'slices', 'Bread'),
+      ],
     ),
     Recipe(
       'Chocolate Chip Cookies',
       'assets/choco_chip.jpeg',
+      24,
+      [
+        Ingredient(4, 'cups', 'flour'),
+        Ingredient(2, 'cups', 'sugar'),
+        Ingredient(0.5, 'cups', 'chocolate chips'),
+      ],
     ),
     Recipe(
       'Taco Salad',
       'assets/taco.jpeg',
+      1,
+      [
+        Ingredient(4, 'oz', 'nachos'),
+        Ingredient(3, 'oz', 'taco meat'),
+        Ingredient(0.5, 'cup', 'cheese'),
+        Ingredient(0.25, 'cup', 'chopped tomatoes'),
+      ],
     ),
     Recipe(
       'Hawaiian Pizza',
       'assets/hawaiian_pizza.jpeg',
+      4,
+      [
+        Ingredient(1, 'item', 'pizza'),
+        Ingredient(1, 'cup', 'pineapple'),
+        Ingredient(8, 'oz', 'ham'),
+      ],
     ),
   ];
+}
+
+class Ingredient{
+  String name;
+  double quantity;
+  String measure;
+
+  Ingredient(this.quantity, this.measure, this.name);
 }
