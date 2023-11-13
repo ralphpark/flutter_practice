5f38f71d5944f6f091b23ecff382fff878ccc35b
ralphpark <ralphpark@knou.ac.kr>
2023-11-14 02:25:05 +0900

    flutter practice update

---
+++ lib/03What_is_the_widget/lib/screen/explore_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('ExploreScreen'),);
+  }
+}
